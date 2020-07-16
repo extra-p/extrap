133bfc211a0e66688de418a4f1c8d155bf9eaadc
Alexander Geiß <geiss@cs.tu-darmstadt.de>
2020-07-16 18:12:56 +0200

    Fixed potential bug, where order of parameters could lead to wrong result

--- src/fileio/talpas_file_reader.py
+++ src/fileio/talpas_file_reader.py
@@ -31,10 +31,11 @@ def read_talpas_file(path, progress_event=lambda _: _):
     experiment = Experiment()
 
     complete_data = {}
+    parameters = None
 
     # read talpas file into complete_data
     with open(path) as file:
-        for line in file:
+        for ln, line in enumerate(file):
             if line.isspace():
                 continue
             line = line.replace(';', ',')
@@ -42,20 +43,23 @@ def read_talpas_file(path, progress_event=lambda _: _):
             try:
                 data = json.loads(line)
             except JSONDecodeError as error:
-                raise FileFormatError(f'Decoding of line failed: {str(error).replace(",", ";")}. Line: "{line}"')
-
-            key = Callpath(data['callpath']), Metric(data['metric'])
-            coordinate = Coordinate([(Parameter(p), v)
-                                     for p, v in data['parameters'].items()])
-            if key in complete_data:
-                if coordinate in complete_data[key]:
-                    complete_data[key][coordinate].append(data['value'])
+                raise FileFormatError(f'Decoding of line {ln} failed: {str(error).replace(",", ";")}. Line: "{line}"')
+            try:
+                key = Callpath(data['callpath']), Metric(data['metric'])
+                if parameters is None:
+                    parameters = [Parameter(p) for p in data['parameters'].keys()]
+                coordinate = Coordinate(data['parameters'][p.name] for p in parameters)
+                if key in complete_data:
+                    if coordinate in complete_data[key]:
+                        complete_data[key][coordinate].append(data['value'])
+                    else:
+                        complete_data[key][coordinate] = [data['value']]
                 else:
-                    complete_data[key][coordinate] = [data['value']]
-            else:
-                complete_data[key] = {
-                    coordinate: [data['value']]
-                }
+                    complete_data[key] = {
+                        coordinate: [data['value']]
+                    }
+            except KeyError as error:
+                raise FileFormatError(f'Missing property in line {ln}: {str(error)}. Line: "{line}"')
 
     # create experiment
     for key in complete_data:
@@ -69,7 +73,7 @@ def read_talpas_file(path, progress_event=lambda _: _):
             experiment.add_measurement(
                 Measurement(coordinate, callpath, metric, values))
 
-    for p in coordinate._parameters:
+    for p in parameters:
         experiment.add_parameter(p)
 
     callpaths = experiment.get_callpaths()
