21097b38ffd9e32cfa9d016ba8920bfc5d01c3bf
Marcus Ritter <19738708+MeaParvitas@users.noreply.github.com>
2020-09-24 16:23:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -34,7 +34,6 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
