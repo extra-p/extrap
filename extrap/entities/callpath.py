3c6e238f8bb1cdf7b7cae87cd3272910e662fb13
Alexander Geiß <geiss@cs.tu-darmstadt.de>
2021-02-12 15:49:18 +0100

    Callpaths with additional info tags

--- extrap/entities/callpath.py
+++ extrap/entities/callpath.py
@@ -6,8 +6,11 @@
 # See the LICENSE file in the base directory for details.
 
 import itertools
+import json
 
 from extrap.util.serialization_schema import make_value_schema
+
+DATA_SEPARATOR = '\x03'
 
 
 class Callpath:
@@ -20,12 +23,30 @@ class Callpath:
     ID_COUNTER = itertools.count()
     EMPTY: 'Callpath'
 
-    def __init__(self, name):
+    def __init__(self, name: str, **tags):
         """
         Initialize callpath object.
         """
-        self.name = name
+
+        self.name = None
+        self.tags = tags or {}
+        self.data = name
         self.id = next(Callpath.ID_COUNTER)
+
+    @property
+    def data(self):
+        if self.tags:
+            return self.name + DATA_SEPARATOR + json.dumps(self.tags)
+        else:
+            return self.name
+
+    @data.setter
+    def data(self, val: str):
+        if DATA_SEPARATOR in val:
+            self.name, tag_string = val.split(DATA_SEPARATOR, 1)
+            self.tags = json.loads(tag_string)
+        else:
+            self.name = val
 
     def __hash__(self):
         return hash(self.name)
@@ -42,5 +63,5 @@ class Callpath:
         return f"Callpath({self.name})"
 
 
-CallpathSchema = make_value_schema(Callpath, 'name')
-Callpath.EMPTY = Callpath(None)
+CallpathSchema = make_value_schema(Callpath, 'data')
+Callpath.EMPTY = Callpath("")
