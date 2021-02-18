03fea5fcf63e26ec528fcc0d622741c2cf283e97
Alexander Geiß <geiss@cs.tu-darmstadt.de>
2021-02-18 13:14:49 +0100

    Unified named entities code

--- extrap/entities/callpath.py
+++ extrap/entities/callpath.py
@@ -1,6 +1,6 @@
 # This file is part of the Extra-P software (http://www.scalasca.org/software/extra-p)
 #
-# Copyright (c) 2020, Technical University of Darmstadt, Germany
+# Copyright (c) 2020-2021, Technical University of Darmstadt, Germany
 #
 # This software may be modified and distributed under the terms of a BSD-style license.
 # See the LICENSE file in the base directory for details.
@@ -8,60 +8,27 @@
 import itertools
 import json
 
+from extrap.entities.named_entity import NamedEntityWithTags
 from extrap.util.serialization_schema import make_value_schema
 
-DATA_SEPARATOR = '\x03'
 
-
-class Callpath:
+class Callpath(NamedEntityWithTags):
     """
     This class represents a callpath of an application.
     """
+
+    TYPENAME = 'Callpath'
+
+    ID_COUNTER = itertools.count()
     """
     Counter for global callpath ids
     """
-    ID_COUNTER = itertools.count()
+
     EMPTY: 'Callpath'
-
-    def __init__(self, name: str, **tags):
-        """
-        Initialize callpath object.
-        """
-
-        self.name = None
-        self.tags = tags or {}
-        self.data = name
-        self.id = next(Callpath.ID_COUNTER)
-
-    @property
-    def data(self):
-        if self.tags:
-            return self.name + DATA_SEPARATOR + json.dumps(self.tags)
-        else:
-            return self.name
-
-    @data.setter
-    def data(self, val: str):
-        if DATA_SEPARATOR in val:
-            self.name, tag_string = val.split(DATA_SEPARATOR, 1)
-            self.tags = json.loads(tag_string)
-        else:
-            self.name = val
-
-    def __hash__(self):
-        return hash(self.name)
-
-    def __eq__(self, other):
-        if not isinstance(other, Callpath):
-            return NotImplemented
-        return self is other or self.name == other.name
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return f"Callpath({self.name})"
+    """
+    Empty callpath. Can be used as placeholder.
+    """
 
 
-CallpathSchema = make_value_schema(Callpath, 'data')
+CallpathSchema = make_value_schema(Callpath, '_data')
 Callpath.EMPTY = Callpath("")
