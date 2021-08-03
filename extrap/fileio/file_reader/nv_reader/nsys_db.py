607822c9c15605705e97733bf6f4bf110b1cd14a
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2021-08-03 19:51:14 +0200

    Add support for extra_prof annotations to nsight reader

--- extrap/fileio/file_reader/nv_reader/nsys_db.py
+++ extrap/fileio/file_reader/nv_reader/nsys_db.py
@@ -10,6 +10,7 @@ from pathlib import Path
 from sqlite3 import Cursor
 from typing import Tuple, List
 
+from extrap.util.deprecation import deprecated
 from extrap.util.exceptions import FileFormatError
 
 
@@ -20,7 +21,8 @@ class NsysReport:
         self.db = sqlite3.connect(filename)
         self._prepare_shared()
 
-    def _check_file(self, filename):
+    @staticmethod
+    def _check_file(filename):
         if filename.suffix == ".qdstrm":
             raise FileFormatError(
                 f"You need to convert the file into a .qdrep-report and than into a sqlite database: {filename}")
@@ -37,21 +39,91 @@ class NsysReport:
             [name]).fetchone() is not None
 
     def _prepare_shared(self):
+        if self._check_table_exists('EXTRAP_RESOLVED_CALLPATHS'):
+            # if necessary upgrade to newer view
+            if self.db.execute("""SELECT name AS CNTREC
+FROM pragma_table_info('EXTRAP_RESOLVED_CALLPATHS')
+WHERE name = 'correlationId'""").fetchone() is None:
+                self.db.execute("DROP VIEW EXTRAP_RESOLVED_CALLPATHS")
+            else:
+                return
+
+        if self._check_table_exists('NVTX_EVENTS'):
+            # if possible use extra_prof data
+            domain_id = self.db.execute(
+                "SELECT domain_id FROM NVTX_EVENTS  WHERE text='de.tu-darmstadt.parallel.extra_prof'").fetchone()
+            if domain_id:
+                domain_id = domain_id[0]
+                self._prepare_callpaths_from_extra_prof(domain_id)
+                return
+
+        self._prepare_callpaths_from_nsys_tracing()
+
+    def _prepare_callpaths_from_nsys_tracing(self):
         self.db.execute("""CREATE VIEW IF NOT EXISTS EXTRAP_RESOLVED_CALLPATHS AS
 WITH resolved_callchains AS (
-    SELECT CUDA_CALLCHAINS.id, symbol, stackDepth, value AS name
+    SELECT CUDA_CALLCHAINS.id, symbol, stackDepth, VALUE AS NAME
     FROM CUDA_CALLCHAINS
              LEFT JOIN StringIds ON symbol = StringIds.id
     WHERE unresolved IS NULL
     ORDER BY CUDA_CALLCHAINS.id, stackDepth DESC
-)
-SELECT id,
-       GROUP_CONCAT(symbol, ',') AS path,
-       MAX(stackDepth),
-       GROUP_CONCAT(name, '->')  AS callpath
-FROM resolved_callchains
-GROUP BY id
+),
+     callchains AS (SELECT id,
+                           GROUP_CONCAT(symbol, ',') AS path,
+                           MAX(stackDepth)           AS stackDepth,
+                           GROUP_CONCAT(name, '->')  AS callpath
+                    FROM resolved_callchains
+                    GROUP BY id)
+SELECT correlationId,
+       path || ',' || nameId                          AS path,
+       callchains.callpath || '->' || StringIds.value AS callpath,
+       stackDepth + 1                                 AS stackDepth,
+       CA.end - CA.start                              AS duration
+FROM callchains
+         INNER JOIN main.CUPTI_ACTIVITY_KIND_RUNTIME AS CA ON callchains.id - 1 = CA.callchainId
+         LEFT JOIN StringIds ON nameId = StringIds.id
+GROUP BY correlationId
          """)
+
+    def _prepare_callpaths_from_extra_prof(self, domain_id):
+        self.db.execute(f"""CREATE VIEW IF NOT EXISTS EXTRAP_RESOLVED_CALLPATHS AS
+WITH nvtx AS (
+    SELECT start, end, value, uint32Value AS depth, textId AS path, globalTid
+    FROM NVTX_EVENTS
+             LEFT JOIN StringIds ON textId = StringIds.id
+    WHERE eventType = 59
+      AND domainId = {domain_id}
+      AND textId IS NOT NULL
+),
+     nvtxrec AS (
+         SELECT *
+         FROM nvtx
+         WHERE depth = 0
+         UNION ALL
+         SELECT nvtx.start,
+                nvtx.end,
+                nvtxrec.value || '->' || nvtx.value,
+                nvtx.depth,
+                nvtxrec.path || ',' || nvtx.path,
+                nvtxrec.globalTid
+         FROM nvtxrec
+                  INNER JOIN nvtx ON nvtxrec.start < nvtx.start AND nvtx.end < nvtxrec.end AND
+                                     nvtxrec.depth = nvtx.depth - 1 AND nvtxrec.globalTid = nvtx.globalTid
+         ORDER BY nvtx.start
+     )
+SELECT correlationId,
+       path || ',' || nameId                    AS path,
+       nvtxrec.value || '->' || StringIds.value AS callpath,
+       MAX(depth) + 1                           AS stackDepth,
+       CA.end - CA.start                        AS duration
+FROM nvtxrec
+         INNER JOIN main.CUPTI_ACTIVITY_KIND_RUNTIME AS CA
+                    ON nvtxrec.start < CA.start AND CA.end < nvtxrec.end AND CA.globalTid = nvtxrec.globalTid
+         LEFT JOIN StringIds ON nameId = id
+GROUP BY correlationId
+UNION ALL
+SELECT NULL AS correlationId, path, value AS callpath, depth AS stackDepth, end - start AS duration
+FROM nvtxrec""")
 
     def get_mem_copies(self) -> List[Tuple[int, str, str, str, float, int, str, float]]:
         if not self._check_table_exists('CUPTI_ACTIVITY_KIND_MEMCPY'):
@@ -59,7 +131,7 @@ GROUP BY id
         c = self.db.cursor()
         return list(c.execute("""WITH cupti_memory AS (
     SELECT correlationId,
-           (end - start) AS duration,
+           (END - START) AS duration,
            CASE copyKind
                WHEN 0 THEN 'UNKNOWN'
                WHEN 1 THEN 'HOST_TO_DEVICE'
@@ -95,27 +167,23 @@ GROUP BY id
                 NULL                                                    AS bytes,
                 shortName
          FROM CUPTI_ACTIVITY_KIND_MEMCPY AS CAKS
-                  INNER JOIN (SELECT start, end, shortName FROM CUPTI_ACTIVITY_KIND_KERNEL) AS CAKK
-                            ON CAKK.start BETWEEN CAKS.start AND CAKS.end OR CAKK.end BETWEEN CAKS.start AND CAKS.end
+                  INNER JOIN (SELECT start, END, shortName FROM CUPTI_ACTIVITY_KIND_KERNEL) AS CAKK
+                             ON CAKK.start BETWEEN CAKS.start AND CAKS.end OR CAKK.end BETWEEN CAKS.start AND CAKS.end
      ),
      cupti_activity AS (
-         SELECT callchainId,
-                value                                                               AS demangledName,
+         SELECT CA.correlationId,
+                NULL        AS demangledName,
                 copyKind,
-                CUPTI_ACTIVITY_KIND_RUNTIME.end - CUPTI_ACTIVITY_KIND_RUNTIME.start AS duration,
                 bytes,
-                CA.duration                                                         AS other_duration
-         FROM CUPTI_ACTIVITY_KIND_RUNTIME
-                  INNER JOIN (SELECT *
-                              FROM cupti_memory
-                              UNION ALL
-                              SELECT *
-                              FROM cupti_memory_overlap
-         ) AS CA
-                             ON CUPTI_ACTIVITY_KIND_RUNTIME.correlationId = CA.correlationId
-                  LEFT JOIN StringIds ON nameId = StringIds.id
+                CA.duration AS other_duration
+         FROM (SELECT *
+               FROM cupti_memory
+               UNION ALL
+               SELECT *
+               FROM cupti_memory_overlap
+              ) AS CA
      )
-SELECT paths.id,
+SELECT paths.correlationId,
        path,
        callpath,
        demangledName       AS name,
@@ -123,7 +191,7 @@ SELECT paths.id,
        SUM(bytes)          AS bytes,
        copyKind,
        SUM(other_duration) AS other_duration
-FROM (SELECT id,
+FROM (SELECT EXTRAP_RESOLVED_CALLPATHS.correlationId,
              path,
              demangledName,
              callpath,
@@ -132,7 +200,8 @@ FROM (SELECT id,
              other_duration,
              bytes
       FROM EXTRAP_RESOLVED_CALLPATHS
-               INNER JOIN cupti_activity ON (id - 1) = CUPTI_ACTIVITY.callchainId) AS paths
+               INNER JOIN cupti_activity
+                          ON EXTRAP_RESOLVED_CALLPATHS.correlationId = CUPTI_ACTIVITY.correlationId) AS paths
 GROUP BY path, demangledName, copyKind 
     """))
 
@@ -142,7 +211,7 @@ GROUP BY path, demangledName, copyKind
         c = self.db.cursor()
         return list(c.execute("""WITH cupti_synchronization AS (
     SELECT correlationId,
-           (end - start) AS duration,
+           (END - START) AS duration,
            NULL          AS durationGPU,
            CASE syncType
                WHEN 0 THEN 'UNKNOWN'
@@ -166,26 +235,24 @@ GROUP BY path, demangledName, copyKind
                     END                                                 AS syncType,
                 shortName
          FROM CUPTI_ACTIVITY_KIND_SYNCHRONIZATION AS CAKS
-                  LEFT JOIN (SELECT start, end, shortName FROM CUPTI_ACTIVITY_KIND_KERNEL) AS CAKK
+                  LEFT JOIN (SELECT start, END, shortName FROM CUPTI_ACTIVITY_KIND_KERNEL) AS CAKK
                             ON CAKK.start BETWEEN CAKS.start AND CAKS.end OR CAKK.end BETWEEN CAKS.start AND CAKS.end
      ),
      cupti_activity AS (
-         SELECT callchainId,
-                value                                                               AS demangledName,
+         SELECT correlationId,
+                value         AS demangledName,
                 syncType,
-                CUPTI_ACTIVITY_KIND_RUNTIME.end - CUPTI_ACTIVITY_KIND_RUNTIME.start AS duration,
                 durationGPU,
-                CAKK.duration                                                       AS other_duration
-         FROM CUPTI_ACTIVITY_KIND_RUNTIME
-                  INNER JOIN (SELECT *
-                             FROM cupti_synchronization
-                             UNION ALL
-                             SELECT *
-                             FROM cupti_synchronization_kernels) AS CAKK
-                            ON CUPTI_ACTIVITY_KIND_RUNTIME.correlationId = CAKK.correlationId
+                CAKK.duration AS other_duration
+         FROM (SELECT *
+               FROM cupti_synchronization
+               UNION ALL
+               SELECT *
+               FROM cupti_synchronization_kernels) AS CAKK
+
                   LEFT JOIN StringIds ON shortName = StringIds.id
      )
-SELECT paths.id,
+SELECT paths.correlationId,
        path,
        callpath,
        demangledName       AS kernelName,
@@ -193,7 +260,7 @@ SELECT paths.id,
        SUM(durationGPU)    AS durationGPU,
        syncType,
        SUM(other_duration) AS other_duration
-FROM (SELECT id,
+FROM (SELECT EXTRAP_RESOLVED_CALLPATHS.correlationId,
              path,
              demangledName,
              callpath,
@@ -202,7 +269,8 @@ FROM (SELECT id,
              syncType,
              other_duration
       FROM EXTRAP_RESOLVED_CALLPATHS
-               INNER JOIN cupti_activity ON (id - 1) = CUPTI_ACTIVITY.callchainId) AS paths
+               INNER JOIN cupti_activity
+                          ON EXTRAP_RESOLVED_CALLPATHS.correlationId = CUPTI_ACTIVITY.correlationId) AS paths
 GROUP BY path, demangledName, syncType 
     """))
 
@@ -213,7 +281,7 @@ GROUP BY path, demangledName, syncType
         return list(c.execute("""WITH cupti_kernel AS (
     SELECT correlationId,
            NULL                                                     AS duration,
-           (end - start)                                            AS durationGPU,
+           (END - START)                                            AS durationGPU,
            shortName,
            ('(' || gridX || ',' || gridY || ',' || gridZ || ')')    AS grid,
            ('(' || blockX || ',' || blockY || ',' || blockZ || ')') AS block,
@@ -221,36 +289,34 @@ GROUP BY path, demangledName, syncType
     FROM CUPTI_ACTIVITY_KIND_KERNEL
 ),
      cupti_activity AS (
-         SELECT callchainId,
-                value                                                               AS demangledName,
+         SELECT correlationId,
+                value         AS demangledName,
                 grid,
                 block,
                 sharedMemoryExecuted,
-                CUPTI_ACTIVITY_KIND_RUNTIME.end - CUPTI_ACTIVITY_KIND_RUNTIME.start AS duration,
                 durationGPU,
-                CAKK.duration                                                       AS other_duration
-         FROM CUPTI_ACTIVITY_KIND_RUNTIME
-                  LEFT JOIN cupti_kernel AS CAKK
-                            ON CUPTI_ACTIVITY_KIND_RUNTIME.correlationId = CAKK.correlationId
+                CAKK.duration AS other_duration
+         FROM cupti_kernel AS CAKK
                   LEFT JOIN StringIds ON shortName = StringIds.id
      )
-SELECT paths.id,
+SELECT paths.correlationId,
        path,
        callpath,
        demangledName       AS kernelName,
        SUM(duration)       AS duration,
        SUM(durationGPU)    AS durationGPU,
        SUM(other_duration) AS other_duration
-FROM (SELECT id,
+FROM (SELECT EXTRAP_RESOLVED_CALLPATHS.correlationId,
              path,
              demangledName,
-             --(demangledName || '<<<' || grid || ',' || block || ',' || sharedMemoryExecuted || '>>>') AS demangledName,
+             --(demangledName|| '<<<' || grid || ',' || block || ',' || sharedMemoryExecuted || '>>>') AS demangledName,
              callpath,
              duration,
              durationGPU,
              other_duration
       FROM EXTRAP_RESOLVED_CALLPATHS
-               LEFT JOIN cupti_activity ON (id - 1) = CUPTI_ACTIVITY.callchainId) AS paths
+               LEFT JOIN cupti_activity
+                         ON EXTRAP_RESOLVED_CALLPATHS.correlationId = CUPTI_ACTIVITY.correlationId) AS paths
 GROUP BY path, demangledName 
 """))
 
@@ -260,7 +326,7 @@ GROUP BY path, demangledName
         return c.execute("""WITH cupti_kernel AS (
     SELECT correlationId,
            gridId,
-           (end - start)                                            AS durationGPU,
+           (END - START)                                            AS durationGPU,
            shortName,
            ('(' || gridX || ',' || gridY || ',' || gridZ || ')')    AS grid,
            ('(' || blockX || ',' || blockY || ',' || blockZ || ')') AS block,
@@ -268,19 +334,16 @@ GROUP BY path, demangledName
     FROM CUPTI_ACTIVITY_KIND_KERNEL
 ),
      cupti_activity AS (
-         SELECT callchainId,
+         SELECT correlationId,
                 gridId,
                 value                                                               AS demangledName,
                 grid,
                 block,
                 sharedMemoryExecuted,
-                CUPTI_ACTIVITY_KIND_RUNTIME.end - CUPTI_ACTIVITY_KIND_RUNTIME.start AS duration,
                 durationGPU
          FROM cupti_kernel AS CAKK
-                  LEFT JOIN CUPTI_ACTIVITY_KIND_RUNTIME
-                            ON CUPTI_ACTIVITY_KIND_RUNTIME.correlationId = CAKK.correlationId
                   LEFT JOIN StringIds ON shortName = StringIds.id
-         WHERE callchainId IS NOT NULL
+         WHERE correlationId IS NOT NULL
      )
 SELECT demangledName AS name,
        grid,
@@ -288,35 +351,38 @@ SELECT demangledName AS name,
        sharedMemoryExecuted,
        callpath
 FROM cupti_activity
-         LEFT JOIN EXTRAP_RESOLVED_CALLPATHS ON (id - 1) = CUPTI_ACTIVITY.callchainId
+         LEFT JOIN EXTRAP_RESOLVED_CALLPATHS ON EXTRAP_RESOLVED_CALLPATHS.correlationId = CUPTI_ACTIVITY.correlationId
 
     """)
 
+    @deprecated
     def get_os_runtimes(self) -> List[Tuple[int, str, str, str, float, float, str, float]]:
-        if not self._check_table_exists('OSRT_API'):
-            return []
-        c = self.db.cursor()
-        return list(c.execute("""WITH osrt_activity AS (
-    SELECT callchainId,
-           end - start AS duration,
-           value       AS name
-    FROM OSRT_API
-             LEFT JOIN StringIds ON nameId = StringIds.id
-)
-SELECT paths.id,
-       path,
-       callpath,
-       name,
-       SUM(duration) AS duration
-FROM (SELECT id,
-             path,
-             name,
-             callpath,
-             duration
-      FROM EXTRAP_RESOLVED_CALLPATHS
-               LEFT JOIN osrt_activity ON id = osrt_activity.callchainId) AS paths
-GROUP BY path, name 
-    """))
+        return []
+
+    #         if not self._check_table_exists('OSRT_API'):
+    #             return []
+    #         c = self.db.cursor()
+    #         return list(c.execute("""WITH osrt_activity AS (
+    #     SELECT callchainId,
+    #            END - START AS duration,
+    #            VALUE       AS NAME
+    #     FROM OSRT_API
+    #              LEFT JOIN StringIds ON nameId = StringIds.id
+    # )
+    # SELECT paths.id,
+    #        path,
+    #        callpath,
+    #        name,
+    #        SUM(duration) AS duration
+    # FROM (SELECT id,
+    #              path,
+    #              name,
+    #              callpath,
+    #              duration
+    #       FROM EXTRAP_RESOLVED_CALLPATHS
+    #                LEFT JOIN osrt_activity ON id = osrt_activity.callchainId) AS paths
+    # GROUP BY path, name
+    #     """))
 
     def __enter__(self):
         return self
