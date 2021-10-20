5f60c91995200528440dbc7e60c32f3ea9b63405
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2021-10-20 11:47:17 +0200

    Edited menu of CallTreeView

--- extrap/gui/TreeView.py
+++ extrap/gui/TreeView.py
@@ -84,8 +84,7 @@ class TreeView(QTreeView):
 
     def contextMenuEvent(self, event):
         menu = QMenu()
-        expand_submenu = menu.addMenu("Expand")
-        collapse_submenu = menu.addMenu("Collapse")
+
         model: TreeModel = self.model()
         if model is not None:
             if self.selectedIndexes():
@@ -94,18 +93,12 @@ class TreeView(QTreeView):
                 if selectedCallpath is None:
                     return
                 selectedModel = model.getSelectedModel(selectedCallpath.path)
-                expandAction = expand_submenu.addAction("Expand all")
+
+                expandAction = menu.addAction("Expand all")
                 expandAction.triggered.connect(self.expandAll)
-                expandSubtree = expand_submenu.addAction("Expand subtree")
-                expandSubtree.triggered.connect(
-                    lambda: self.expandRecursively(self.selectedIndexes()[0]))
-                expandLargest = expand_submenu.addAction("Expand largest")
-                expandLargest.triggered.connect(lambda: self.expand_largest(model, self.selectedIndexes()[0]))
-                collapseAction = collapse_submenu.addAction("Collapse all")
-                collapseAction.triggered.connect(self.collapseAll)
-                collapseSubtree = collapse_submenu.addAction("Collapse subtree")
-                collapseSubtree.triggered.connect(
-                    lambda: self.collapseRecursively(self.selectedIndexes()[0]))
+
+                menu.addMenu(self._create_expand_collapse_menu(model))
+                menu.addSeparator()  # --------------------------------------------------
                 # showCommentsAction = menu.addAction("Show Comments")
                 # showCommentsAction.setEnabled(
                 #     selectedModel is not None and len(selectedModel.getComments()) > 0)
@@ -121,6 +114,23 @@ class TreeView(QTreeView):
                     lambda: self.copy_model_to_clipboard(selectedModel)
                 )
                 menu.exec_(self.mapToGlobal(event.pos()))
+
+    def _create_expand_collapse_menu(self, model):
+        expand_collapse_submenu = QMenu("Expand / Collapse")
+        expandAction2 = expand_collapse_submenu.addAction("Expand all")
+        expandAction2.triggered.connect(self.expandAll)
+        expandSubtree = expand_collapse_submenu.addAction("Expand subtree")
+        expandSubtree.triggered.connect(
+            lambda: self.expandRecursively(self.selectedIndexes()[0]))
+        expandLargest = expand_collapse_submenu.addAction("Expand largest")
+        expandLargest.triggered.connect(lambda: self.expand_largest(model, self.selectedIndexes()[0]))
+        expand_collapse_submenu.addSeparator()  # --------------------------------------------------
+        collapseAction = expand_collapse_submenu.addAction("Collapse all")
+        collapseAction.triggered.connect(self.collapseAll)
+        collapseSubtree = expand_collapse_submenu.addAction("Collapse subtree")
+        collapseSubtree.triggered.connect(
+            lambda: self.collapseRecursively(self.selectedIndexes()[0]))
+        return expand_collapse_submenu
 
     def copy_model_to_clipboard(self, selectedModel):
         parameters = self.model().main_widget.getExperiment().parameters
