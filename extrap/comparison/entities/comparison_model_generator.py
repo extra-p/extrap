f3fa8ddf2f068b663e3f3e99fa3709429f62213a
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2021-10-21 13:02:47 +0200

    Add missing license headers

--- extrap/comparison/entities/comparison_model_generator.py
+++ extrap/comparison/entities/comparison_model_generator.py
@@ -1,3 +1,10 @@
+# This file is part of the Extra-P software (http://www.scalasca.org/software/extra-p)
+#
+# Copyright (c) 2021, Technical University of Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of a BSD-style license.
+# See the LICENSE file in the base directory for details.
+
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
