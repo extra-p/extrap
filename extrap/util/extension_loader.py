748443ed2b9c4c2c570ce6b41009f4854a3ec489
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2021-11-24 16:20:05 +0100

    Fix potential bug in extension loader

--- extrap/util/extension_loader.py
+++ extrap/util/extension_loader.py
@@ -4,7 +4,7 @@
 #
 # This software may be modified and distributed under the terms of a BSD-style license.
 # See the LICENSE file in the base directory for details.
-
+import importlib
 import inspect
 import pkgutil
 from typing import TypeVar, MutableMapping, Iterator, Type, Callable
@@ -50,10 +50,9 @@ def load_extensions(path: str, pkg_name: str, type_: Type[_VT], post_process: Ca
                and not inspect.isabstract(x)
 
     modelers = CaseInsensitiveStringDict()
-    for importer, modname, is_pkg in pkgutil.walk_packages(path=path,
-                                                           prefix=pkg_name + '.',
-                                                           onerror=lambda x: None):
-        module = importer.find_module(modname).load_module(modname)
+    for _, modname, _ in pkgutil.walk_packages(path=path, prefix=pkg_name + '.', onerror=lambda x: None):
+        # Do not use the returned importer. This may create two different classes with the same name and members.
+        module = importlib.import_module(modname)
         for name, clazz in inspect.getmembers(module, is_modeler):
             clazz: Type[_VT]
             name = clazz.NAME
