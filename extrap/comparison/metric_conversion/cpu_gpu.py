b2f27375089a1e1f5c5813f8cbed5efeba65b0b3
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2021-12-07 10:11:21 +0100

    Add load/store CPU-GPU comparison

--- extrap/comparison/metric_conversion/cpu_gpu.py
+++ extrap/comparison/metric_conversion/cpu_gpu.py
@@ -37,3 +37,29 @@ class FlopsSP(AbstractMetricConverter):
     def _conversion2(self, inst_executed_op_add: CalculationElement, inst_executed_op_mul: CalculationElement,
                      inst_executed_op_fma: CalculationElement) -> CalculationElement:
         return inst_executed_op_add + inst_executed_op_mul + 2 * inst_executed_op_fma
+
+
+class VecLoads(AbstractMetricConverter):
+    NAME = "VEC LOADS"
+
+    @ConversionMetrics("PAPI_LD_INS", "PAPI_SP_OPS", "PAPI_DP_OPS", "PAPI_VEC_SP", "PAPI_VEC_DP")
+    def _conversion1(self, ld_ins: CalculationElement, sp_ops: CalculationElement, dp_ops: CalculationElement,
+                     vec_sp_ops: CalculationElement, vec_dp_ops: CalculationElement) -> CalculationElement:
+        return ld_ins * ((vec_sp_ops + vec_dp_ops) / (dp_ops + sp_ops))
+
+    @ConversionMetrics("smsp__sass_inst_executed_op_ld")
+    def _conversion2(self, inst_executed_op_ld: CalculationElement, ) -> CalculationElement:
+        return inst_executed_op_ld
+
+
+class VecStores(AbstractMetricConverter):
+    NAME = "VEC STORES"
+
+    @ConversionMetrics("PAPI_SR_INS", "PAPI_SP_OPS", "PAPI_DP_OPS", "PAPI_VEC_SP", "PAPI_VEC_DP")
+    def _conversion1(self, sr_ins: CalculationElement, sp_ops: CalculationElement, dp_ops: CalculationElement,
+                     vec_sp_ops: CalculationElement, vec_dp_ops: CalculationElement) -> CalculationElement:
+        return sr_ins * ((vec_sp_ops + vec_dp_ops) / (dp_ops + sp_ops))
+
+    @ConversionMetrics("smsp__sass_inst_executed_op_st")
+    def _conversion2(self, inst_executed_op_st: CalculationElement, ) -> CalculationElement:
+        return inst_executed_op_st
