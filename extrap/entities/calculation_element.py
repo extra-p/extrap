f765cb9f5e60cf4bddebd46a70d374c1d60c6d16
Alexander Geiß <alexander@geiss1.de>
2022-05-18 21:32:37 +0200

    Fix Python 3.7 compatibility issue

--- extrap/entities/calculation_element.py
+++ extrap/entities/calculation_element.py
@@ -7,8 +7,9 @@
 
 from __future__ import annotations
 
+from typing import  Union, TypeVar
 try:
-    from typing import Protocol, Union, TypeVar
+    from typing import Protocol
 except ImportError:
     class Protocol:
         pass
