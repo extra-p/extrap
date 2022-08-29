ae7863e4591a1c9c57d9562ed3933a9208337cd5
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2022-08-29 11:42:28 +0200

    Update version number

--- extrap/__init__.py
+++ extrap/__init__.py
@@ -6,7 +6,7 @@
 # See the LICENSE file in the base directory for details.
 
 __title__ = "Extra-P"
-__version__ = "4.1.0-alpha"
+__version__ = "4.3.0-alpha1"
 __description__ = "Extra-P, automated performance modeling for HPC applications"
 __copyright__ = "Copyright (c) 2020-2022 Technical University of Darmstadt, Darmstadt, Germany"
 __documentation_link__ = f"https://github.com/extra-p/extrap/tree/v{__version__}/docs"
