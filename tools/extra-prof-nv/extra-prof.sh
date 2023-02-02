15212ec59c4878d010a04cfbe9b616e207444e9c
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2023-02-02 11:21:40 +0100

    Fix bug in extra-prof

--- tools/extra-prof-nv/extra-prof.sh
+++ tools/extra-prof-nv/extra-prof.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 if [ -z ${EXTRA_PROF_EXPERIMENT_DIRECTORY+x} ]; then
-    EXTRA_PROF_EXPERIMENT_DIRECTORY=$EXTRA_PROF_EXPERIMENT_DIRECTORY
-elif [ -z ${SCOREP_EXPERIMENT_DIRECTORY+x} ]; then
-    EXTRA_PROF_EXPERIMENT_DIRECTORY=$SCOREP_EXPERIMENT_DIRECTORY
-else
-    EXTRA_PROF_EXPERIMENT_DIRECTORY="extra_prof_result$(date)"
+    if [ -z ${SCOREP_EXPERIMENT_DIRECTORY+x} ]; then
+        EXTRA_PROF_EXPERIMENT_DIRECTORY="extra_prof_result$(date)"
+    else
+        EXTRA_PROF_EXPERIMENT_DIRECTORY=$SCOREP_EXPERIMENT_DIRECTORY
+    fi
 fi
-nsys profile -o "$EXTRA_PROF_EXPERIMENT_DIRECTORY/profile%q{SLURM_PROCID}" --export sqlite -b dwarf --cudabacktrace none -s none -t cuda,nvtx -x true -f true "$@"
+exec nsys profile -o "$EXTRA_PROF_EXPERIMENT_DIRECTORY/profile%q{SLURM_PROCID}" --export sqlite -b dwarf --cudabacktrace none -s none -t cuda,nvtx -x true -f true "$@"
