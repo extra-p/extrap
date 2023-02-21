cfe059723d2e9f66657ad7850c196fda1bd4b996
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2023-02-21 12:58:06 +0100

    Improve Extra-Prof wrapper scripts

--- tools/extra-prof-nv/nvcc-wrapper.sh
+++ tools/extra-prof-nv/nvcc-wrapper.sh
@@ -25,7 +25,7 @@ while test $# -gt 0; do
     shift
 done
 
-instrumentation_arguments=("-Xcompiler" "-finstrument-functions" "-Xcompiler" "-no-pie" "-Xlinker" "--no-pie")
+instrumentation_arguments=("-Xcompiler" "-finstrument-functions" "-Xcompiler" "-no-pie" "-Xlinker" "-no-pie")
 extra_prof_arguments=("-c" "-O2" "$compiler_dir" "-std" "c++17" "-I$(dirname "${BASH_SOURCE[0]}")/msgpack/include")
 if test "${EXTRA_PROF_EVENT_TRACE}" = on || test "${EXTRA_PROF_EVENT_TRACE}" = ON; then
     extra_prof_arguments+=("-DEXTRA_PROF_EVENT_TRACE=1")
@@ -33,26 +33,30 @@ fi
 
 if $compile_only; then
     combined=("${instrumentation_arguments[@]}" "${arguments[@]}")
-    echo "EXTRA PROF CALL: " $COMPILER ${combined[*]}
+    echo "EXTRA PROF COMPILE: " $COMPILER ${combined[*]}
     exec $COMPILER ${combined[*]}
 else
     if [ -d "$(dirname "${BASH_SOURCE[0]}")/msgpack" ]; then
-        echo "Found msgpack"
+        echo "EXTRA PROF: Found msgpack"
     else
-        echo "Downloading msgpack..."
+        echo "EXTRA PROF: Downloading msgpack..."
         wget -nv -O"$(dirname "${BASH_SOURCE[0]}")/msgpack-cxx.tar.gz" "https://github.com/msgpack/msgpack-c/releases/download/cpp-$MSGPACK_VERSION/msgpack-cxx-$MSGPACK_VERSION.tar.gz"
-        tar -xzf -C "$(dirname "${BASH_SOURCE[0]}")" "$(dirname "${BASH_SOURCE[0]}")/msgpack-cxx.tar.gz"
+        [ $? -eq 0 ] || exit $?
+        echo "EXTRA PROF: Unpacking msgpack..."
+        tar -xzf "$(dirname "${BASH_SOURCE[0]}")/msgpack-cxx.tar.gz" -C "$(dirname "${BASH_SOURCE[0]}")"
+        [ $? -eq 0 ] || exit $?
         mv "$(dirname "${BASH_SOURCE[0]}")/msgpack-cxx-$MSGPACK_VERSION" "$(dirname "${BASH_SOURCE[0]}")/msgpack"
-        echo "Finished unpacking msgpack. Continuing..."
+        [ $? -eq 0 ] || exit $?
+        echo "EXTRA PROF: Finished unpacking msgpack. Continuing..."
     fi
 
     # no exec otherwise this script will end here
     combined=("${extra_prof_arguments[@]}" "-o" "extra_prof_instrumentation.o" "$(dirname "${BASH_SOURCE[0]}")/extra_prof/instrumentation.cpp")
-    echo "EXTRA PROF CALL: " $COMPILER ${combined[*]}
+    echo "EXTRA PROF COMPILE INSTRUMENTATION: " $COMPILER ${combined[*]}
     $COMPILER ${combined[*]}
     [ $? -eq 0 ] || exit $?
 
-    combined=("${instrumentation_arguments[@]}" "-lcupti" "extra_prof_instrumentation.o" "${arguments[@]}")
+    combined=("${instrumentation_arguments[@]}" "-lcupti" "-L" "$CUDA_HOME/extras/CUPTI/lib64" "extra_prof_instrumentation.o" "${arguments[@]}")
     echo "EXTRA PROF CALL: " $COMPILER ${combined[*]}
     exec $COMPILER ${combined[*]}
 fi
