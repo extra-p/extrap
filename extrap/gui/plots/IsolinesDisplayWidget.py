83e325f8c232d6b2e4fcf8fd0aeea1cec42e2ffd
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2023-06-30 18:50:49 +0200

    Fix large empty space in Isolines Plot

--- extrap/gui/plots/IsolinesDisplayWidget.py
+++ extrap/gui/plots/IsolinesDisplayWidget.py
@@ -32,24 +32,6 @@ class IsolinesDisplay(BaseContourGraph):
 
         X, Y, Z_List, z_List = self.calculate_z_models(maxX, maxY, model_list)
 
-        # Get the callpath color map
-        widget = self.main_widget
-        dict_callpath_color = widget.model_color_map
-        number_of_subplots = 1
-        if len(Z_List) > 1:
-            number_of_subplots = len(Z_List) + 1
-
-        # Adjusting subplots in order to avoid overlapping of labels
-        # Reference : https://stackoverflow.com/questions/2418125/matplotlib-subplots-adjust-hspace-so-titles-and-xlabels-dont-overlap
-        left = 0.1
-        right = 0.9
-        bottom = 0.2
-        top = 0.9
-        wspace = 0.5
-        hspace = 0.2
-        self.fig.subplots_adjust(
-            left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
-
         # Set the x_label and y_label based on parameter selected.
         x_label = self.main_widget.data_display.getAxisParameter(0).name
         if x_label.startswith("_"):
@@ -58,23 +40,36 @@ class IsolinesDisplay(BaseContourGraph):
         if y_label.startswith("_"):
             y_label = y_label[1:]
 
-        # Set the axis details for the subplot where we will draw all isolines
-        ax_all = self.fig.add_subplot(
-            1, number_of_subplots, number_of_subplots)
-        ax_all.xaxis.major.formatter._useMathText = True
-        ax_all.yaxis.major.formatter._useMathText = True
-        ax_all.set_xlabel('\n' + x_label)
-        ax_all.set_ylabel('\n' + y_label)
-        ax_all.set_title(r'All')
-        for item in ([ax_all.title, ax_all.xaxis.label, ax_all.yaxis.label]):
-            item.set_fontsize(10)
+        # Get the callpath color map
+        widget = self.main_widget
+        dict_callpath_color = widget.model_color_map
+        number_of_subplots = 1
+        if len(Z_List) > 1:
+            number_of_subplots = len(Z_List) + 1
+
+            # Adjusting subplots in order to avoid overlapping of labels
+            # Reference : https://stackoverflow.com/questions/2418125/matplotlib-subplots-adjust-hspace-so-titles-and-xlabels-dont-overlap
+            # left = 0.1
+            # right = 0.9
+            # bottom = 0.2
+            # top = 0.9
+            # wspace = 0.5
+            # hspace = 0.2
+            # self.fig.subplots_adjust(
+            #     left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+
+            # Set the axis details for the subplot where we will draw all isolines
+            ax_all = self.fig.add_subplot(
+                1, number_of_subplots, number_of_subplots)
+            ax_all.xaxis.major.formatter._useMathText = True
+            ax_all.yaxis.major.formatter._useMathText = True
+            ax_all.set_xlabel('\n' + x_label)
+            ax_all.set_ylabel('\n' + y_label)
+            ax_all.set_title(r'All')
 
         # Draw isolines
         for i in range(len(Z_List)):
-            if i == 0:
-                ax = ax_all
-            else:
-                ax = self.fig.add_subplot(1, number_of_subplots, i + 1)
+            ax = self.fig.add_subplot(1, number_of_subplots, i + 1)
             ax.xaxis.major.formatter._useMathText = True
             ax.yaxis.major.formatter._useMathText = True
             try:
@@ -89,7 +84,9 @@ class IsolinesDisplay(BaseContourGraph):
             ax.set_ylabel('\n' + y_label)
 
             # ax.set_title ('function'+ str(i+1))
-            # ax.set_title(functions[i])
+            ax.set_title(selected_callpaths[i].name,
+                         fontdict={'fontsize': self.main_widget.plot_formatting_options.legend_font_size})
+
             if len(Z_List) > 1:
                 try:
                     cs_all = ax_all.contour(
@@ -99,13 +96,14 @@ class IsolinesDisplay(BaseContourGraph):
                         fontsize=self.main_widget.plot_formatting_options.font_size * 0.8)
                 except ValueError:  # raised if function selected is constant
                     pass
-            self.fig.subplots_adjust(
-                left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
+            # self.fig.subplots_adjust(
+            #     left=left, bottom=bottom, right=right, top=top, wspace=wspace, hspace=hspace)
             # for item in ([ax.title, ax.xaxis.label, ax.yaxis.label]):
             #     item.set_fontsize(10)
             # self.fig.colorbar(CS, ax=ax)
             # cax = ax.imshow(Z_List[i], interpolation='nearest', cmap=cm.coolwarm)
             # self.fig.colorbar(cax)
 
-        # draw legend
-        self.draw_legend(ax_all, dict_callpath_color)
+        if len(Z_List) > 1:
+            # draw legend
+            self.draw_legend(ax_all, dict_callpath_color)
