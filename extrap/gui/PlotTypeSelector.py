38dbb200c5506d11e3df23d56cc6e99d1da40557
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2023-06-30 18:51:14 +0200

    Fix layout of plot type selector

--- extrap/gui/PlotTypeSelector.py
+++ extrap/gui/PlotTypeSelector.py
@@ -12,7 +12,6 @@ class PlotTypeSelector(QDialog):
 
     def __init__(self, parent, dataDisplay):
         super(PlotTypeSelector, self).__init__(parent)
-        self.valid = False
         self.dataDisplay = dataDisplay
         self.init_UI()
 
@@ -22,31 +21,25 @@ class PlotTypeSelector(QDialog):
 
         plotTypes = ['Line graph', 'Selected models in same surface plot', 'Selected models in different surface plots',
                      'Dominating models in a 3D Scatter plot', 'Max z as a single surface plot',
-                     'Dominating models and max z as heat map', ' Selected models in contour plot',
+                     'Dominating models and max z as heat map', 'Selected models in contour plot',
                      'Selected models in interpolated contour plots', 'Measurement points']
 
-        self.checkBoxes = [QCheckBox(plotType, self) for plotType in plotTypes]
+        layout = QVBoxLayout()
 
-        y_cord = 10
-        for checkBox in self.checkBoxes:
-            checkBox.move(10, y_cord)
-            y_cord = y_cord + 30
+        self.checkBoxes = []
+        for plotType in plotTypes:
+            check_box = QCheckBox(plotType, self)
+            self.checkBoxes.append(check_box)
+            layout.addWidget(check_box)
 
-        self.setFixedHeight(y_cord + 70)
+        dialog_buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        dialog_buttons.accepted.connect(self.accept)
+        dialog_buttons.rejected.connect(self.reject)
+        layout.addWidget(dialog_buttons)
 
-        ok_button = QPushButton(self)
-        ok_button.setText("OK")
-        ok_button.move(165, y_cord + 30)
-        ok_button.setFixedWidth(110)
-        ok_button.pressed.connect(self.ok_pressed)
+        self.setLayout(layout)
 
-        cancel_button = QPushButton(self)
-        cancel_button.setText("Cancel")
-        cancel_button.move(40, y_cord + 30)
-        cancel_button.setFixedWidth(100)
-        cancel_button.pressed.connect(self.close)
-
-    def ok_pressed(self):
+    def accept(self):
         numberOfCheckBox = len(self.checkBoxes)
         selectedCheckBoxesIndex = list()
 
@@ -54,6 +47,5 @@ class PlotTypeSelector(QDialog):
             if self.checkBoxes[count].isChecked():
                 selectedCheckBoxesIndex.append(count)
 
-        self.valid = True
-        self.close()
         self.dataDisplay.reloadTabs(selectedCheckBoxesIndex)
+        super().accept()
