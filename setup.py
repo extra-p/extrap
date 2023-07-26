959cadf5ff596651adc10097686c5bed59e901be
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2023-07-26 15:34:22 +0200

    CI; update requirements of packaging package

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         "tqdm~=4.47",
         "pycubexr>=1.1,<3",
         "marshmallow~=3.7",
-        "packaging~=20.0",
+        "packaging>=20.0,<22",
         "kaitaistruct~=0.9",
         "protobuf~=3.14",
         "itanium_demangler~=1.0",
