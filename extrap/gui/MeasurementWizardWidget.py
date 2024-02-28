41fd3e62e30edff2ad5bf9926b43e74f9d7ac5fb
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2024-02-28 13:09:08 +0100

    Fix Bug: Rename self.layout to self._layout to prevent issues with shadowing self.layout()

--- extrap/gui/MeasurementWizardWidget.py
+++ extrap/gui/MeasurementWizardWidget.py
@@ -37,37 +37,36 @@ class MeasurementWizardWidget(QWidget):
                                  'Enable <b>keep values</b> during the import.')
         self.main_layout.addWidget(self.empty_label)
 
-    
     def init_multi_parameter(self):
         self.initialized = True
 
         self.no_model_parameters = len(self.experiment.parameters)
-    
+
         self.main_layout.removeWidget(self.empty_label)
         if self.empty_label is not None:
             self.empty_label.deleteLater()
             self.empty_label = None
 
-        self.layout = QGridLayout(self)
-        self.layout.setRowStretch(99, 1)
-        self.layout.setColumnStretch(0, 0)
-        self.layout.setColumnStretch(1, 1)
+        self._layout = QGridLayout(self)
+        self._layout.setRowStretch(99, 1)
+        self._layout.setColumnStretch(0, 0)
+        self._layout.setColumnStretch(1, 1)
 
-        self.main_layout.addLayout(self.layout)
+        self.main_layout.addLayout(self._layout)
 
         self.checkbox = QCheckBox("No. of processes (MPI ranks)\n is one of the model parameters", self)
         self.checkbox.setChecked(True)
         self.checkbox.toggled.connect(self.clickCheckbox)
-        self.layout.addWidget(self.checkbox, 0, 0)
+        self._layout.addWidget(self.checkbox, 0, 0)
 
         self.parameter_selector_label = QLabel(self)
         self.parameter_selector_label.setText("Model Paramater resembling the\n number of processes (MPI ranks):")
-        self.layout.addWidget(self.parameter_selector_label, 1, 0)
+        self._layout.addWidget(self.parameter_selector_label, 1, 0)
 
         self.parameter_selector = QComboBox(self)
         for parameter in self.experiment.parameters:
             self.parameter_selector.addItem(str(parameter))
-        self.layout.addWidget(self.parameter_selector, 1, 1)
+        self._layout.addWidget(self.parameter_selector, 1, 1)
         for i in range(len(self.experiment.parameters)):
             if self.parameter_selector.currentText() == str(self.experiment.parameters[i]):
                 self.processes_parameter_id = i
@@ -76,92 +75,91 @@ class MeasurementWizardWidget(QWidget):
 
         self.process_label = QLabel(self)
         self.process_label.setText("No. of processes (MPI ranks):")
-        self.layout.addWidget(self.process_label, 2, 0)
+        self._layout.addWidget(self.process_label, 2, 0)
         self.process_label.setVisible(False)
 
         self.processes = QLineEdit(self)
         self.processes.setValidator(QIntValidator())
-        self.layout.addWidget(self.processes, 2, 1)
+        self._layout.addWidget(self.processes, 2, 1)
         self.processes.setVisible(False)
         self.processes.setText("2")
         self.processes.textChanged.connect(self.processesChanged)
 
         metric_label = QLabel(self)
         metric_label.setText("Metric used for cost calculation:")
-        self.layout.addWidget(metric_label, 3, 0)
+        self._layout.addWidget(metric_label, 3, 0)
 
         self.metric_selector = QComboBox(self)
         for metric in self.experiment.metrics:
             self.metric_selector.addItem(str(metric))
-        self.layout.addWidget(self.metric_selector, 3, 1)
+        self._layout.addWidget(self.metric_selector, 3, 1)
         self.metric_selector.currentIndexChanged.connect(self.metricChanged)
 
         budget_label = QLabel(self)
         budget_label.setText("Modeling Budget [core hours]:")
-        self.layout.addWidget(budget_label, 4, 0)
+        self._layout.addWidget(budget_label, 4, 0)
 
         self.modeling_budget = QDoubleSpinBox(self)
         self.modeling_budget.setMaximum(999999999999.0)
         self.modeling_budget.setMinimum(0.0)
-        self.layout.addWidget(self.modeling_budget, 4, 1)
+        self._layout.addWidget(self.modeling_budget, 4, 1)
         self.modeling_budget.textChanged.connect(self.budgetChanged)
 
         current_cost_label = QLabel(self)
         current_cost_label.setText("Current Measurement Cost [core hours]:")
-        self.layout.addWidget(current_cost_label, 5, 0)
+        self._layout.addWidget(current_cost_label, 5, 0)
 
         self.current_cost_edit = QLineEdit(self)
         self.current_cost_edit.setReadOnly(True)
         self.current_cost_edit.setText("0.0")
-        self.layout.addWidget(self.current_cost_edit, 5, 1)
+        self._layout.addWidget(self.current_cost_edit, 5, 1)
 
         used_budget_label = QLabel(self)
         used_budget_label.setText("Used Modeling Budget [%]:")
-        self.layout.addWidget(used_budget_label, 6, 0)
+        self._layout.addWidget(used_budget_label, 6, 0)
 
         self.used_budget_edit = QLineEdit(self)
         self.used_budget_edit.setReadOnly(True)
         self.used_budget_edit.setText("0.0")
-        self.layout.addWidget(self.used_budget_edit, 6, 1)
+        self._layout.addWidget(self.used_budget_edit, 6, 1)
 
         noise_level_label = QLabel(self)
         noise_level_label.setText("Measurement Noise Level [%]:")
-        self.layout.addWidget(noise_level_label, 7, 0)
+        self._layout.addWidget(noise_level_label, 7, 0)
 
         self.noise_level_edit = QLineEdit(self)
         self.noise_level_edit.setReadOnly(True)
         noise_level_str = "{:.2f}".format(self.mean_noise_level)
         self.noise_level_edit.setText(noise_level_str)
-        self.layout.addWidget(self.noise_level_edit, 7, 1)
+        self._layout.addWidget(self.noise_level_edit, 7, 1)
 
         self.parameter_value_checkbox = QCheckBox("Enter parameter-value series manually", self)
         self.parameter_value_checkbox.setChecked(False)
         self.parameter_value_checkbox.toggled.connect(self.clickParameterValueCheckbox)
-        self.layout.addWidget(self.parameter_value_checkbox, 8, 0)
+        self._layout.addWidget(self.parameter_value_checkbox, 8, 0)
 
         self.param_value_layout = QGridLayout(self)
         self.param_value_layout.setRowStretch(99, 1)
         self.param_value_layout.setColumnStretch(0, 0)
         self.param_value_layout.setColumnStretch(1, 1)
-        self.layout.addLayout(self.param_value_layout, 9, 0)
+        self._layout.addLayout(self.param_value_layout, 9, 0)
 
         self.advise_button = QPushButton(self)
-        self.layout.addWidget(self.advise_button, 10, 0)
+        self._layout.addWidget(self.advise_button, 10, 0)
         self.advise_button.setText("Suggest Additional Measurement Points")
         self.advise_button.clicked.connect(self.advice_button_clicked)
 
         suggestion_label = QLabel(self)
         suggestion_label.setText("Measurement point suggestions:")
-        self.layout.addWidget(suggestion_label, 11, 0)
+        self._layout.addWidget(suggestion_label, 11, 0)
 
         self.measurement_point_suggestions_label = QPlainTextEdit(self)
         self.measurement_point_suggestions_label.setPlainText("")
         self.measurement_point_suggestions_label.setReadOnly(True)
-        self.layout.addWidget(self.measurement_point_suggestions_label, 11, 1)
+        self._layout.addWidget(self.measurement_point_suggestions_label, 11, 1)
 
         self.calculate_current_measurement_cost()
         self.calculate_noise_level()
-
 
     def init_single_parameter(self):
         self.initialized = True
@@ -171,107 +169,106 @@ class MeasurementWizardWidget(QWidget):
             self.empty_label.deleteLater()
             self.empty_label = None
 
-        self.layout = QGridLayout(self)
-        self.layout.setRowStretch(99, 1)
-        self.layout.setColumnStretch(0, 0)
-        self.layout.setColumnStretch(1, 1)
+        self._layout = QGridLayout(self)
+        self._layout.setRowStretch(99, 1)
+        self._layout.setColumnStretch(0, 0)
+        self._layout.setColumnStretch(1, 1)
 
-        self.main_layout.addLayout(self.layout)
+        self.main_layout.addLayout(self._layout)
 
         self.checkbox = QCheckBox("Model parameters is the no.\n of processes (MPI ranks)", self)
         self.checkbox.setChecked(True)
         self.checkbox.toggled.connect(self.clickCheckbox)
-        self.layout.addWidget(self.checkbox, 0, 0)
+        self._layout.addWidget(self.checkbox, 0, 0)
         self.processes_parameter_id = 0
 
         self.process_label = QLabel(self)
         self.process_label.setText("No. of processes (MPI ranks):")
-        self.layout.addWidget(self.process_label, 1, 0)
+        self._layout.addWidget(self.process_label, 1, 0)
         self.process_label.setVisible(False)
 
         self.processes = QLineEdit(self)
         self.processes.setValidator(QIntValidator())
-        self.layout.addWidget(self.processes, 1, 1)
+        self._layout.addWidget(self.processes, 1, 1)
         self.processes.setVisible(False)
         self.processes.setText("2")
         self.processes.textChanged.connect(self.processesChanged)
 
         metric_label = QLabel(self)
         metric_label.setText("Metric used for cost calculation:")
-        self.layout.addWidget(metric_label, 2, 0)
+        self._layout.addWidget(metric_label, 2, 0)
 
         self.metric_selector = QComboBox(self)
         for metric in self.experiment.metrics:
             self.metric_selector.addItem(str(metric))
-        self.layout.addWidget(self.metric_selector, 2, 1)
+        self._layout.addWidget(self.metric_selector, 2, 1)
         self.metric_selector.currentIndexChanged.connect(self.metricChanged)
 
         budget_label = QLabel(self)
         budget_label.setText("Modeling Budget [core hours]:")
-        self.layout.addWidget(budget_label, 3, 0)
+        self._layout.addWidget(budget_label, 3, 0)
 
         self.modeling_budget = QDoubleSpinBox(self)
         self.modeling_budget.setMaximum(999999999999.0)
         self.modeling_budget.setMinimum(0.0)
-        self.layout.addWidget(self.modeling_budget, 3, 1)
+        self._layout.addWidget(self.modeling_budget, 3, 1)
         self.modeling_budget.textChanged.connect(self.budgetChanged)
 
         current_cost_label = QLabel(self)
         current_cost_label.setText("Current Measurement Cost [core hours]:")
-        self.layout.addWidget(current_cost_label, 4, 0)
+        self._layout.addWidget(current_cost_label, 4, 0)
 
         self.current_cost_edit = QLineEdit(self)
         self.current_cost_edit.setReadOnly(True)
         self.current_cost_edit.setText("0.0")
-        self.layout.addWidget(self.current_cost_edit, 4, 1)
+        self._layout.addWidget(self.current_cost_edit, 4, 1)
 
         used_budget_label = QLabel(self)
         used_budget_label.setText("Used Modeling Budget [%]:")
-        self.layout.addWidget(used_budget_label, 5, 0)
+        self._layout.addWidget(used_budget_label, 5, 0)
 
         self.used_budget_edit = QLineEdit(self)
         self.used_budget_edit.setReadOnly(True)
         self.used_budget_edit.setText("0.0")
-        self.layout.addWidget(self.used_budget_edit, 5, 1)
+        self._layout.addWidget(self.used_budget_edit, 5, 1)
 
         noise_level_label = QLabel(self)
         noise_level_label.setText("Measurement Noise Level [%]:")
-        self.layout.addWidget(noise_level_label, 6, 0)
+        self._layout.addWidget(noise_level_label, 6, 0)
 
         self.noise_level_edit = QLineEdit(self)
         self.noise_level_edit.setReadOnly(True)
         noise_level_str = "{:.2f}".format(self.mean_noise_level)
         self.noise_level_edit.setText(noise_level_str)
-        self.layout.addWidget(self.noise_level_edit, 6, 1)
+        self._layout.addWidget(self.noise_level_edit, 6, 1)
 
         self.parameter_value_checkbox = QCheckBox("Enter parameter-value series manually", self)
         self.parameter_value_checkbox.setChecked(False)
         self.parameter_value_checkbox.toggled.connect(self.clickParameterValueCheckbox)
-        self.layout.addWidget(self.parameter_value_checkbox, 7, 0)
+        self._layout.addWidget(self.parameter_value_checkbox, 7, 0)
 
         self.param_value_layout = QGridLayout(self)
         self.param_value_layout.setRowStretch(99, 1)
         self.param_value_layout.setColumnStretch(0, 0)
         self.param_value_layout.setColumnStretch(1, 1)
-        self.layout.addLayout(self.param_value_layout, 8, 0)
+        self._layout.addLayout(self.param_value_layout, 8, 0)
 
-        self.advise_button= QPushButton(self)
+        self.advise_button = QPushButton(self)
         self.advise_button.setText("Suggest Additional Measurement Points")
-        self.layout.addWidget(self.advise_button, 9, 0)
+        self._layout.addWidget(self.advise_button, 9, 0)
         self.advise_button.clicked.connect(self.advice_button_clicked)
 
         suggestion_label = QLabel(self)
         suggestion_label.setText("Measurement point suggestions:")
-        self.layout.addWidget(suggestion_label, 10, 0)
+        self._layout.addWidget(suggestion_label, 10, 0)
 
         self.measurement_point_suggestions_label = QPlainTextEdit(self)
         self.measurement_point_suggestions_label.setPlainText("")
         self.measurement_point_suggestions_label.setReadOnly(True)
-        self.layout.addWidget(self.measurement_point_suggestions_label, 10, 1)
+        self._layout.addWidget(self.measurement_point_suggestions_label, 10, 1)
 
         self.calculate_current_measurement_cost()
         self.calculate_noise_level()
-
 
     def calculate_noise_level(self):
         # do an noise analysis on the existing measurement points
