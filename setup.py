8f75cd1fabb7ef255f87ab1de29034048c158bd0
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2024-06-18 14:58:32 +0200

    Add python 3.12 to the supported versions

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
