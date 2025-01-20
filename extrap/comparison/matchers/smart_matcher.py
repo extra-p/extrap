b889ddf64e26b145528717d3a034d67cd7b68ba9
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2025-01-20 18:20:56 +0100

    Update copyright

--- extrap/comparison/matchers/smart_matcher.py
+++ extrap/comparison/matchers/smart_matcher.py
@@ -1,6 +1,6 @@
 # This file is part of the Extra-P software (http://www.scalasca.org/software/extra-p)
 #
-# Copyright (c) 2021-2023, Technical University of Darmstadt, Germany
+# Copyright (c) 2021-2024, Technical University of Darmstadt, Germany
 #
 # This software may be modified and distributed under the terms of a BSD-style license.
 # See the LICENSE file in the base directory for details.
