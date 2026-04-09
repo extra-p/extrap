27112671bc9c28772d245407276671e2a73bcfe2
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2026-04-09 16:21:05 +0200

    Update license headers

--- extrap/__init__.py
+++ extrap/__init__.py
@@ -1,6 +1,6 @@
 # This file is part of the Extra-P software (http://www.scalasca.org/software/extra-p)
 #
-# Copyright (c) 2020-2024, Technical University of Darmstadt, Germany
+# Copyright (c) 2020-2026, Technical University of Darmstadt, Germany
 #
 # This software may be modified and distributed under the terms of a BSD-style license.
 # See the LICENSE file in the base directory for details.
@@ -8,7 +8,7 @@
 __title__ = "Extra-P"
 __version__ = "4.2.3"
 __description__ = "Extra-P, automated performance modeling for HPC applications"
-__copyright__ = "Copyright (c) 2020-2024 Technical University of Darmstadt, Darmstadt, Germany"
+__copyright__ = "Copyright (c) 2020-2026 Technical University of Darmstadt, Darmstadt, Germany"
 __documentation_link__ = f"https://github.com/extra-p/extrap/tree/v{__version__}/docs"
 __current_version_api__ = "https://pypi.org/pypi/extrap/json"
 __developed_by_html__ = 'Extra-P is developed by <a href="https://www.parallel.informatik.tu-darmstadt.de/">' \
